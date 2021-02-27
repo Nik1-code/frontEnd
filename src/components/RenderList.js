494ff344460371c409d44ac914eac8a22a6b71ad
Development <Development@Suyashs-MacBook-Pro.local>
2021-02-27 12:46:32 +0530

    Added RenderList file with sample data

---
+++ src/components/RenderList.js
@@ -0,0 +1,32 @@
+const data = [
+  { name: "Leanne Graham", email: "Sincere@april.biz", id: 1 },
+  { name: "Ervin Howell", email: "Shanna@melissa.tv", id: 2 },
+  { name: "Clementine Bauch", email: "Nathan@yesenia.net", id: 3 },
+  {
+    name: "Patricia Lebsack",
+    email: "Julianne.OConner@kory.org",
+    id: 4,
+  },
+  {
+    name: "Chelsey Dietrich",
+    email: "Lucio_Hettinger@annie.ca",
+    id: 5,
+  },
+  {
+    name: "Mrs. Dennis Schulist",
+    email: "Karley_Dach@jasper.info",
+    id: 6,
+  },
+  { name: "Kurtis Weissnat", email: "Telly.Hoeger@billy.biz", id: 7 },
+  {
+    name: "Nicholas Runolfsdottir V",
+    email: "Sherwood@rosamond.me",
+    id: 8,
+  },
+  { name: "Glenna Reichert", email: "Chaim_McDermott@dana.io", id: 9 },
+  {
+    name: "Clementina DuBuque",
+    email: "Rey.Padberg@karina.biz",
+    id: 10,
+  },
+];
