eecfa416240380f434007cb90e592f7e6617af41
Nik1-code <nik66998@gmail.com>
2021-02-27 12:09:36 -0800

    create password page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,72 +1,108 @@
 import React, { Component } from "react";
+import "./login.css";
 
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: "",
+  };
 
-
-class Login extends Component{
-    
-    state = {
-        email:'',
-        password:'',
-        emailError:'',
-        passwordError:''
+  //email validation
+  submit() {}
+  isValidEmail(email) {
+    console.log(email);
+    if (email === "") {
+      this.setState({ emailError: " Email cannot be empty" });
+      return false;
+    } else {
+      const emailRegEx = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/;
+      const isValidEmail = emailRegEx.test(email);
+      if (!isValidEmail) {
+        this.setState({ emailError: "please enter valid email" });
+        return false;
+      } else {
+        return true;
+      }
     }
-        
-        
-    submit(){
-        if(!this.state.email.includes('@') && this.state.password.length < 6){
-            this.setState({emailError: 'please enter valid email.',passwordError: 'please enter valid password.' })
-        }
-        else if(!this.state.email.includes('@')) {
-            this.setState({emailError: 'please enter valid email.'})
-            }
-        else if(this.state.password.includes('/[a-zA-Z0-9]{6,12}/')) {
-                this.setState({passwordError: 'please enter valid password.'})
-            }
-            else{
-                return true
-            }}
-        // if(!this.state.email.includes('@') && (!this.state.password.includes('@'))){
-        //     this.setState({emailError: 'please enter the valid email address', passwordError: 'a) password minimum length 6 characters b) password maximum length 12 characters c) password has at least 1 upper case, 1 lower case alphabet and 1 digit'})
-        
-        //    ///[a-zA-Z0-9]{6,12}/
-        // } else{
-        //         return true
-          
-        //     }
-        // }
+  }
 
-    login(){
+  onChangeEmail(e) {
+    this.setState({ email: e.target.value, emailError: "" });
+  }
+
+  //password validation
+  isValidPassword(password) {
+    if (password === "") {
+      this.setState({ passwordError: "Password cannot be empty." });
+      return false;
+    } else {
+      const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{6,12}$/;
+      const isValidPassword = passwordRegEx.test(password);
+      if (!isValidPassword) {
         this.setState({
-            emailError:'', passwordError:''
-        })
-        
-        if(this.submit()){
-         alert('Login Successful!')
-          } else{
-         alert('Please try again!')
-          }
+          passwordError:
+            "Password length should be min 6 and max 12 letters with atleast 1 uppercase and 1 lowercase letter and 1 digit.",
+        });
+        return false;
+      } else {
+        return true;
+      }
     }
-    render(){
-        console.log(this.state.email)
-        return(
-        <div>
-            <div className='App-header' id='Login'>
-                <h1 className='App-text'>This is Login Page!</h1>
-                <h2 style={{paddingTop:'20px'}}>Login </h2>
+  }
 
-                <input style={{marginTop:'15px'}} type='email' placeholder='email'
-                 onChange={(event)=>{this.setState({email:event.target.value})}}  />
-                 <h3 style={{color:'red',fontSize:'15px',paddingTop:'5px'}}>{this.state.emailError}</h3>
-                <input  type='password' style={{marginTop:'15px'}} placeholder='password'
-                onChange={(event)=>{this.setState({email:event.target.value})}}  />
-                 <h3 style={{color:'red',fontSize:'15px',paddingTop:'5px'}}>{this.state.passwordError}</h3>
+  onChangePassword(e) {
+    this.setState({ password: e.target.value, passwordError: "" });
+    console.log(e.target.value);
+  }
 
-                <button style={{marginTop:'20px'}} onClick={()=>this.login()}>Login</button>  
-            </div>
+  //login
+  //   login() {
+  //     const emailFlag = this.isValidEmail(this.state.email);
+  //     if (emailFlag) {
+  //       alert("successful");
+  //     }
+  //   }
+  login() {
+    const emailFlag = this.isValidEmail(this.state.email);
+    const passwordFlag = this.isValidPassword(this.state.password);
+    if (emailFlag && passwordFlag) {
+      alert("successful");
+    }
+  }
+  render() {
+    // console.log(this.state.email);
+    return (
+      <div>
+        <div className="App-header" id="Login">
+          <h1 className="App-text">This is Login Page!</h1>
+          <h2 className="loginName">Login </h2>
+
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(event) => this.onChangeEmail(event)}
+          />
+          <h3 className="showError">{this.state.emailError}</h3>
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(event) => this.onChangePassword(event)}
+          />
+          <h3 className="showError">{this.state.passwordError}</h3>
+
+          <button
+            className="testClass"
+            onClick={() => this.login()}
+            disabled={false}
+          >
+            Login
+          </button>
         </div>
-        )
-    }
-
+      </div>
+    );
+  }
 }
 
 export default Login;
